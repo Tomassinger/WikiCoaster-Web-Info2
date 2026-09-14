3c737cbce429bfde9191da84509ef98ed34420e2
Tom Friedelmeyer <friedelmeyer.tom@univ-lorraine.fr>
2026-09-14 15:36:07 +0200

    installation finale

---
+++ app/src/Controller/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class BaseController extends AbstractController
+{
+    /**
+     * page d'accueil
+     */
+    #[Route('/')]
+    public function home(): Response
+    {
+        return $this->render('base/home.html.twig');
+    }
+    
+}
